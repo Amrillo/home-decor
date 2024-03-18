ef007fe133869a65d22117f28f9c3f12ad26afcd
Amrillo <amrishka88@gmail.com>
2024-03-18 18:15:36 +0500

    favorite page

--- frontend/src/app/views/personal/favorite/favorite.component.ts
+++ frontend/src/app/views/personal/favorite/favorite.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FavoriteService } from 'src/app/shared/services/favorite.service';
+import { environment } from 'src/environments/environment';
+import { DefaultResponseType } from 'src/types/default-response.type';
+import { FavoriteType } from 'src/types/favorite.type';
 
 @Component({
   selector: 'app-favorite',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FavoriteComponent implements OnInit {
 
-  constructor() { }
+  products: FavoriteType[] = [];
+  
+  serverStaticPath = environment.serverStaticPath ;
+  constructor(private favoriteService : FavoriteService) { }
 
   ngOnInit(): void {
+      this.favoriteService.getFavorites()
+        .subscribe((data: FavoriteType[] | DefaultResponseType)=> {  
+           if((data as DefaultResponseType).error !== undefined) {  
+              const error  = (data as DefaultResponseType).message; 
+              throw new Error(error); 
+           }       
+
+           this.products = data as FavoriteType[];  
+
+        })
+
   }
+
 
 }
