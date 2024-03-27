4779a451f58daef17807fb1428cbe77c1fe45288
Amrillo <amrishka88@gmail.com>
2024-03-27 18:05:46 +0500

    update order page

--- frontend/src/app/views/order/order.module.ts
+++ frontend/src/app/views/order/order.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { OrderRoutingModule } from './order-routing.module';
 import { CartComponent } from './cart/cart.component';
 import { OrderComponent } from './order/order.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { CarouselModule } from 'ngx-owl-carousel-o';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatDialogModule} from '@angular/material/dialog';
 
 
 @NgModule({
@@ -17,6 +18,8 @@ import { CarouselModule } from 'ngx-owl-carousel-o';
     CommonModule,
     OrderRoutingModule,
     SharedModule, 
+    ReactiveFormsModule,
+    MatDialogModule,
     CarouselModule
   ]
 })
--- frontend/src/app/views/order/order/order.component.ts
+++ frontend/src/app/views/order/order/order.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { CartService } from 'src/app/shared/services/cart.service';
+import { CartType } from 'src/types/cart.type';
+import { DefaultResponseType } from 'src/types/default-response.type';
+import { DeliveryType } from 'src/types/delivery.type';
+import { PaymentType } from 'src/types/payment.type';
 
 @Component({
   selector: 'app-order',
@@ -7,9 +15,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
-  constructor() { }
+  deliveryType: DeliveryType = DeliveryType.delivery;  
+  deliveryTypes = DeliveryType ;  
+  paymentTypes = PaymentType ; 
+  cart: CartType | null = null ; 
+  totalAmount:number = 0;
+  totalCount: number = 0;
+
+  orderForm = this.fb.group( { 
+    firstName: ['',Validators.required],  
+    lastName:['',Validators.required],
+    phone: ['',Validators.required],
+    fatherName: [''],
+    paymentType: [PaymentType.cashToCourier, Validators.required],
+    email: ['',[Validators.required, Validators.email]],
+    street: [''],
+    house: [''],
+    entrance: [''],
+    apartment: [''],
+    comment: [''],
+  } 
+
+  )
+  constructor(private cartService : CartService, private router : Router,  private _snackBar: MatSnackBar,
+    private fb: FormBuilder) { }
 
   ngOnInit(): void {
+
+    this.cartService.getCart()
+    .subscribe((data:CartType | DefaultResponseType)=> {
+      if((data as DefaultResponseType).error !== undefined) {  
+        throw new Error((data as DefaultResponseType).message);
+      }
+      this.cart = data as CartType ;
+      if(!this.cart || (this.cart && this.cart.items.length === 0)) {  
+          this._snackBar.open('Корзина пустая')
+          this.router.navigate(['/'])
+         return 
+      }
+      this.calculateTotal();
+    })
   }
 
+  calculateTotal() {
+    this.totalAmount = 0;
+    this.totalCount = 0;
+    if(this.cart) {
+      this.cart?.items.forEach(item=> {
+        this.totalAmount += item.quantity * item.product.price;
+        this.totalCount += item.quantity;
+      })
+    }
+  }
+
+  changeDeliveryType(type: DeliveryType) {  
+      this.deliveryType = type ; 
+   }
+
+   createOrder(){  
+     if(this.orderForm.valid) { 
+      console.log(this.orderForm.value);
+     }
+   }
 }
