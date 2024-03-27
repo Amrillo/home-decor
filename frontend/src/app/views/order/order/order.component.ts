93aa6756b231bb2c0acb170be4e5a789d59515f2
Akhmedov Amrillo <amrishka88@gmail.com>
2024-03-27 22:30:50 +0500

    update order page

--- frontend/src/app/views/order/order/order.component.ts
+++ frontend/src/app/views/order/order/order.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, ElementRef, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { CartService } from 'src/app/shared/services/cart.service';
+import { OrderService } from 'src/app/shared/services/order.service';
 import { CartType } from 'src/types/cart.type';
 import { DefaultResponseType } from 'src/types/default-response.type';
 import { DeliveryType } from 'src/types/delivery.type';
+import { OrderType } from 'src/types/order.type';
 import { PaymentType } from 'src/types/payment.type';
 
 @Component({
@@ -15,15 +19,15 @@ import { PaymentType } from 'src/types/payment.type';
 })
 export class OrderComponent implements OnInit {
 
-  deliveryType: DeliveryType = DeliveryType.delivery;  
-  deliveryTypes = DeliveryType ;  
-  paymentTypes = PaymentType ; 
-  cart: CartType | null = null ; 
+  deliveryType: DeliveryType = DeliveryType.delivery;
+  deliveryTypes = DeliveryType ;
+  paymentTypes = PaymentType ;
+  cart: CartType | null = null ;
   totalAmount:number = 0;
   totalCount: number = 0;
 
-  orderForm = this.fb.group( { 
-    firstName: ['',Validators.required],  
+  orderForm = this.fb.group( {
+    firstName: ['',Validators.required],
     lastName:['',Validators.required],
     phone: ['',Validators.required],
     fatherName: [''],
@@ -34,24 +38,25 @@ export class OrderComponent implements OnInit {
     entrance: [''],
     apartment: [''],
     comment: [''],
-  } 
+  } )
 
-  )
+  @ViewChild('popup') popup!: TemplateRef<ElementRef>;
+  dialogRef: MatDialogRef<any> | null = null ;
   constructor(private cartService : CartService, private router : Router,  private _snackBar: MatSnackBar,
-    private fb: FormBuilder) { }
+    private fb: FormBuilder, private dialog: MatDialog, private orderService: OrderService) { }
 
   ngOnInit(): void {
 
     this.cartService.getCart()
     .subscribe((data:CartType | DefaultResponseType)=> {
-      if((data as DefaultResponseType).error !== undefined) {  
+      if((data as DefaultResponseType).error !== undefined) {
         throw new Error((data as DefaultResponseType).message);
       }
       this.cart = data as CartType ;
-      if(!this.cart || (this.cart && this.cart.items.length === 0)) {  
+      if(!this.cart || (this.cart && this.cart.items.length === 0)) {
           this._snackBar.open('Корзина пустая')
           this.router.navigate(['/'])
-         return 
+         return
       }
       this.calculateTotal();
     })
@@ -68,13 +73,51 @@ export class OrderComponent implements OnInit {
     }
   }
 
-  changeDeliveryType(type: DeliveryType) {  
-      this.deliveryType = type ; 
+  changeDeliveryType(type: DeliveryType) {
+      this.deliveryType = type ;
    }
 
-   createOrder(){  
-     if(this.orderForm.valid) { 
-      console.log(this.orderForm.value);
+   createOrder(){
+
+     if(this.orderForm.valid && this.orderForm.value.firstName && this.orderForm.value.lastName && this.orderForm.value.phone,
+      this.orderForm.value.paymentType && this.orderForm.value.email) {
+
+      const paramsObject : OrderType = {
+        deliveryType: this.deliveryType,
+        firstName: this.orderForm.value.firstName,
+        lastName: this.orderForm.value.lastName,
+        phone: this.orderForm.value.phone,
+        paymentType: this.orderForm.value.paymentType,
+        email: this.orderForm.value.email,
+      };
+
+      this.orderService.createOrder(paramsObject)
+      .subscribe( {
+        next:(data: OrderType | DefaultResponseType)=> {
+            if((data as DefaultResponseType).error !== undefined) {
+              throw new Error((data as DefaultResponseType).message);
+            }
+            this.dialogRef =  this.dialog.open(this.popup);
+            this.dialogRef.backdropClick()
+              .subscribe( ()=> {
+               this.router.navigate(['/']);
+               }
+             );
+        },
+        error: (errorResponse: HttpErrorResponse)=> {
+            if(errorResponse.error && errorResponse.error.message) {
+                this._snackBar.open(errorResponse.error.message);
+            } else {
+              this._snackBar.open('Oshibka zakaza');
+            }
+        }
+       }
+      )
      }
+   }
+
+   closePopup() {
+      this.dialogRef?.close();
+      this.router.navigate(['/']);
    }
 }
